b93be779df62d1c160cf8ffceac8e1a8aa9c75c6
Taha Bouhsine <skywolf.mo@gmail.com>
2025-07-03 16:40:04 -0700

    removed the redandancy

--- src/models/aether/aethergpt.py
+++ src/models/aether/aethergpt.py
@@ -47,44 +47,6 @@ class MiniGPT(GPT):
             x = transformer_block(x, training=training)
         return self.output_layer(x)
 
-
-@register_model("mini-aethergpt")
-class MiniGPT(GPT):
-    def __init__(self, config: ModelConfig, mesh: Mesh, *, rngs: nnx.Rngs):
-        super().__init__(config, mesh)
-        self.embedding_layer = TokenAndPositionEmbedding(config, rngs=rngs)
-        self.transformer_blocks = [
-            TransformerBlock(config, mesh, rngs=rngs) for _ in range(config.num_transformer_blocks)
-        ]
-        
-        # Handle partitioning based on whether mesh is available
-        if mesh is not None:
-            kernel_init = nnx.with_partitioning(nnx.initializers.orthogonal(), NamedSharding(mesh, P(None, 'model')))
-            alpha_init = nnx.with_partitioning(nnx.initializers.ones_init(), NamedSharding(mesh, P(None, 'model')))
-            bias_init = nnx.with_partitioning(nnx.initializers.zeros_init(), NamedSharding(mesh, P('model')))
-        else:
-            kernel_init = nnx.initializers.orthogonal()
-            alpha_init = nnx.initializers.ones_init()
-            bias_init = nnx.initializers.zeros_init()
-        if config.use_yatnmn:
-            self.output_layer = YatNMN(
-                in_features=config.embed_dim,
-                out_features=config.vocab_size,
-                kernel_init=kernel_init,
-                alpha_init=alpha_init,
-                bias_init=bias_init,
-                use_bias=False,
-                rngs=rngs
-            )
-        else:
-            self.output_layer = nnx.Linear(config.embed_dim, config.vocab_size, kernel_init=kernel_init, bias_init=bias_init, rngs=rngs)
-
-    def __call__(self, inputs, training: bool = False):
-        x = self.embedding_layer(inputs)
-        for transformer_block in self.transformer_blocks:
-            x = transformer_block(x, training=training)
-        return self.output_layer(x)
-
 @register_model("mini-CygnusGPT")
 class MiniCygnusGPT(GPT):
     def __init__(self, config: ModelConfig, mesh: Mesh, *, rngs: nnx.Rngs):
